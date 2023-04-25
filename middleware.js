3f8c9f319404ad05749428f2a4568a71848678ca
Sanjubeboy <sanjubeboy@gmail.com>
2023-04-25 10:38:40 +0530

    Initial commit

---
+++ middleware.js
@@ -0,0 +1,95 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+  response.send("FIND PAGE");
+});
+
+// let listitem = [
+//   {
+//     id: 1,
+//     name: "Akshith",
+//   },
+
+//   {
+//     id: 2,
+//     name: "sanjay",
+//   },
+
+//   {
+//     id: 3,
+//     name: "kumar",
+//   },
+
+//   {
+//     id: 4,
+//     name: "kousik",
+//   }
+// ];
+
+// router.get("/:id", (request, response) => {
+//   const id = Number(request.params.id);
+
+//   const output = listitem.find((items) => items.id === id);
+
+//   if (output) response.json(output.name);
+//   else response.status(500).send("Unknown Error!");
+// }); 
+
+const middle = (req, res, next, id) => {
+  req.message = "Hii this is middleware!!"
+  // req.details = {}
+  next()
+}
+
+router.route('/:uid')
+
+
+
+
+  
+  .get(middle,(request , response) => {
+    console.log(request.details.name)
+    // response.send(`Get method on user page with id ${request.params.id} with name ${request.user.name}`)
+    response.send(request.details)
+    // response.send(request.message)
+  })
+  
+  .put((request , response) => {
+    console.log(request.user)
+    response.send(`Put method on user page with id ${request.params.id}`)
+  })
+  
+  .delete((request , response) => {
+    console.log(request.user)
+    response.send(`Delete method on user page with id ${request.params.id}`)
+  })
+  
+
+
+
+
+const users = [
+  {
+    name:'Web Development',
+    age:200000
+  },
+  {
+    name:'kumar',
+    age:40
+  }
+]
+
+
+
+
+//middleware executes between request and response.
+router.param("uid", (request , response, next, userid) => {
+  console.log(request.params.id)
+  request.details = users[userid]
+  next()
+})
+
+
+module.exports = router;  
---
+++ services.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+  response.send("FIND PAGE");
+});
+
+let listitem = [
+  {
+    id: 1,
+    name: "Web Development",
+  },
+
+  {
+    id: 2,
+    name: "Software Development",
+  },
+
+  {
+    id: 3,
+    name: "App Development",
+  },
+
+  {
+    id: 4,
+    name: "Nothing",
+  }
+];
+
+router.get("/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const output = listitem.find((items) => items.id === id);
+
+  if (output) response.json(output.name);
+  else response.status(500).send("Unknown Error!");
+}); 
+
+
+module.exports = router;  
